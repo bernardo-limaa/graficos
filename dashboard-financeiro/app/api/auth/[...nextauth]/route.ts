aba5092e701597cf3954d5da49df574ae129b8e1
Bernardo Lima <96505072+bernardo-limaa@users.noreply.github.com>
2025-07-10 16:08:13 +0000

    consertando bug

--- dashboard-financeiro/app/api/auth/[...nextauth]/route.ts
+++ dashboard-financeiro/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,6 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  pages: {
-    signIn: "/login",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
